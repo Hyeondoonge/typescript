c805c2b79691ae5f0f30d2d8784a2a95af578894
Hyeondoonge <migu554@naver.com>
2023-11-06 14:01:11 +0900

    keyof Type Operator 내용 추가

--- advanced/keyof Type Operator.ts
+++ advanced/keyof Type Operator.ts
@@ -3,3 +3,25 @@
 // 1. 일반 type 또는 interface에 keyof 사용하기
 type People = { name: string; age: number; gender: 'male' | 'female' };
 type keyofPeople = keyof People; // 'name' | 'age' | 'gender'
+interface Animal {
+  name: string;
+  age: number;
+}
+
+type keyofAnimal = keyof Animal; // 'age' | 'name'
+
+// 2. 인덱스 시그니처에 keyof 사용하기
+type Arraylish = { [n: number]: unknown };
+type A = keyof Arraylish; // number
+
+type Mapish = { [k: string]: boolean };
+type M = keyof Mapish; // string | number
+
+// 위와 같은 경우 string이 아닌 string | number가 되는 이유는,
+// js의 객체 키는 항상 문자열 타입으로 강제되기 때문이다.
+
+// 따라서 key가 0인 값을 넣어도 "0"으로 식별되기 때문에 number타입의 키를 허용한다.
+
+const map: Mapish = { 0: false, '1': false, name: false };
+
+export {};
