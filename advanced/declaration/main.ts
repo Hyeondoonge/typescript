9f4d0d4337090407af9e71d9c52c063224d7cace
audrey <audrey@mobidoc.us>
2025-06-08 22:56:21 +0900

    declaration - library 타입 선언

---
+++ advanced/declaration/main.ts
@@ -0,0 +1,5 @@
+import { makeGreeting } from 'mylib'
+
+let result = makeGreeting('hello, world!')
+console.log('The computed greeting is: ' + result)
+console.log('mylib author is: : ' + author.name)
